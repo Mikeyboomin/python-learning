30780d2b0671b5ce2c0f0cc3a4c09e18e6034ab7
Boomin <info.mikeyboomin@gmail.com>
2026-04-11 02:52:30 +0100

    added new file

---
+++ pythoncrashcourse/guest_list.py
@@ -0,0 +1,8 @@
+guests = ['milla', 'tessa', 'franca']
+
+message = 'Hey ' + guests[0].title() + ', I just wanted to invite you to my party.'
+print(message)
+message = 'Hey ' + guests[1].title() + ', I just wanted to invite you to my party.'
+print(message)
+message = 'Hey ' + guests[2].title() + ', I just wanted to invite you to my party.'
+print(message)
